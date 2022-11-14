d7ef863e92fb6ce310c2127c935a5ab6e41cbc2d
Inés Sabaidini <sabaidinii@gmail.com>
2022-11-14 00:51:09 -0300

    Cree el repositorio y guardé mi documento

---
+++ PrimerMVT.py
@@ -0,0 +1,10 @@
+#Desafío Entregable
+#Mi Primer MVT
+#Sabaidini Inés
+
+#Detalle del paso a paso
+
+#CONECTANDO A GIT Y CREANDO MI REPOSITORIO
+#Abro una terminal de GitBash en la carpeta donde está ubicado mi proyecto
+#git init
+#git add PrimerMVT.py
