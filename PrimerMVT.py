c07133651f6440c9f4fa8d5fcbb5382bd031ec06
Inés Sabaidini <sabaidinii@gmail.com>
2022-11-14 01:06:46 -0300

    Conecté Django y agregué el archivo de views

--- PrimerMVT.py
+++ PrimerMVT.py
@@ -8,3 +8,19 @@
 #Abro una terminal de GitBash en la carpeta donde está ubicado mi proyecto
 #git init
 #git add PrimerMVT.py
+#git commit -m 'Cree el repositorio y guardé mi documento'
+
+#Subo mi repositorio a GitHub
+#git remote add origin https://github.com/inesabaidini/PrimerMVT.git
+#git branch -M main
+#git push -u origin main
+
+#ARRANCANDO CON DJANGO
+#django-admin startproject primermvt
+#cd primermvt
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py runserver
+
+#VIEWS
+#Agregi views.py dentro de la carpeta primermvt
