b72695d0a1d8caa4080bb6adb7683763adfc5b61
Inés Sabaidini <sabaidinii@gmail.com>
2022-11-14 21:00:43 -0300

    Agregue las vistas dentro de mi aplicación

--- primermvt/familia/views.py
+++ primermvt/familia/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from familia.models import Familia
+
+def familia(request):
+    listado_familia = Familia.objects.all()
+    tabla = {'lista_familia': listado_familia}
+    plantilla = loader.get_template('plantilla.html')
+    documento = plantilla.render(tabla)
+    return HttpResponse(documento)
